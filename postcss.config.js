8364bc86b79439855891b6180198f249e8c9e15f
Hiroki Osame <hiroki.osame@gmail.com>
2018-10-15 23:54:50 +0900

    init

---
+++ postcss.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  plugins: {
+    autoprefixer: {},
+    "postcss-preset-env": {
+      stage: 0
+    }
+  }
+};
---
+++ src/SplitView.vue
@@ -0,0 +1,190 @@
+<template>
+  <div
+    ref="container"
+    :class="[$s.SplitView, {
+      [$s.isVertical]: isVertical
+    }]"
+  >
+    <div
+      :class="[$s.SideA, {
+        [$s.isLocked]: isDragging
+      }]"
+      :style="styleA"
+    >
+      <slot name="A" />
+    </div>
+    <span
+      :class="[$s.Handle, {
+        [$s.isVertical]: isVertical,
+        [$s.isHorizontal]: !isVertical
+      }]"
+      @mousedown.prevent="dragStart"
+    />
+    <div :class="[$s.SideB, {
+        [$s.isLocked]: isDragging
+      }]">
+      <slot name="B" />
+    </div>
+  </div>
+</template>
+
+<script>
+function getPosition(element) {
+  let xPosition = 0;
+  let yPosition = 0;
+
+  while (element) {
+    xPosition += element.offsetLeft - element.scrollLeft + element.clientLeft;
+    yPosition += element.offsetTop - element.scrollTop + element.clientTop;
+    element = element.offsetParent;
+  }
+
+  return { x: xPosition, y: yPosition };
+}
+
+export default {
+  name: "SplitView",
+
+  props: {
+    direction: {
+      type: String,
+      default: "horizontal",
+      validator: direction => ["vertical", "horizontal"].includes(direction)
+    },
+    AInit: {
+      type: String,
+      default: "50%"
+    },
+    AMin: {
+      type: String,
+      default: "none"
+    },
+    AMax: {
+      type: String,
+      default: "none"
+    }
+  },
+
+  data() {
+    return {
+      isDragging: false,
+      offset: this.AInit
+    };
+  },
+
+  computed: {
+    isVertical() {
+      return this.direction === "vertical";
+    },
+    offsetA() {
+      if (typeof this.offset === "string") {
+        return this.offset;
+      }
+      return `${this.offset}px`;
+    },
+    styleA() {
+      const property = this.isVertical ? "Height" : "Width";
+      return {
+        [property.toLowerCase()]: this.offsetA,
+        [`min${property}`]: this.AMin,
+        [`max${property}`]: this.AMax
+      };
+    }
+  },
+
+  methods: {
+    dragStart() {
+      this.isDragging = true;
+      window.addEventListener("mousemove", this.dragging, { passive: true });
+      window.addEventListener("mouseup", this.dragStop, {
+        passive: true,
+        once: true
+      });
+    },
+
+    dragStop() {
+      window.removeEventListener("mousemove", this.dragging);
+      this.isDragging = false;
+    },
+
+    mouseOffset(e) {
+      const { container } = this.$refs;
+      const containerOffset = getPosition(container);
+      let offset;
+
+      if (this.isVertical) {
+        offset = e.pageY - containerOffset.y;
+        offset = Math.min(offset, container.offsetHeight);
+      } else {
+        offset = e.pageX - containerOffset.x;
+        offset = Math.min(offset, container.offsetWidth);
+      }
+
+      return Math.max(offset, 0);
+    },
+
+    dragging(e) {
+      this.offset = this.mouseOffset(e);
+    }
+  }
+};
+</script>
+
+<style module="$s">
+.SplitView {
+  position: relative;
+  display: flex;
+  width: 100%;
+  height: 100%;
+
+  &.isVertical {
+    flex-direction: column;
+  }
+
+  &.isLocked {
+    pointer-events: none;
+  }
+}
+
+.Handle {
+  user-select: none;
+  box-sizing: border-box;
+  transition: all 0.3s ease;
+  z-index: 1;
+  background: padding-box #000a;
+
+  &.isHorizontal {
+    width: 11px;
+    border-left: 5px solid transparent;
+    border-right: 5px solid transparent;
+    margin: 0 -5px;
+    cursor: col-resize;
+  }
+
+  &.isVertical {
+    height: 11px;
+    border-top: 5px solid transparent;
+    border-bottom: 5px solid transparent;
+    margin: -5px 0;
+    cursor: row-resize;
+  }
+
+  &:hover,
+  &:active {
+    border-color: #0004;
+  }
+
+  &:active {
+    border-width: 4px;
+  }
+}
+
+.SideA {
+  overflow: auto;
+}
+
+.SideB {
+  composes: SideA;
+  flex: 1;
+}
+</style>
