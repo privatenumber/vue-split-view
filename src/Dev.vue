97267017cc4a8fc1e4c5400bfac4ca9ea82ac298
Hiroki Osame <hiroki.osame@gmail.com>
2020-12-16 17:51:48 -0500

    refactor!: use rollup

--- src/Dev.vue
+++ src/Dev.vue
@@ -1,104 +1,104 @@
 <template>
-  <div id="app">
-    <div :class="$s.Container">
-      <split-view direction="vertical">
-        <template slot="A">
-          Slot A
-        </template>
-        <template slot="B">
-          Slot B
-        </template>
-      </split-view>
-    </div>
+	<div id="app">
+		<div :class="$s.Container">
+			<split-view direction="vertical">
+				<template slot="A">
+					Slot A
+				</template>
+				<template slot="B">
+					Slot B
+				</template>
+			</split-view>
+		</div>
 
-    <div :class="$s.Container">
-      <split-view direction="horizontal">
-        <template slot="A">
-          Slot A
-        </template>
-        <template slot="B">
-          Slot B
-        </template>
+		<div :class="$s.Container">
+			<split-view direction="horizontal">
+				<template slot="A">
+					Slot A
+				</template>
+				<template slot="B">
+					Slot B
+				</template>
 
-      </split-view>
-    </div>
+			</split-view>
+		</div>
 
-    <div :class="$s.Container">
-      <split-view direction="horizontal">
-        <template slot="A">
-          Slot A
-        </template>
-        <split-view
-          slot="B"
-          direction="vertical"
-        >
-          <template slot="A">
-            Slot A
-          </template>
-          <template slot="B">
-            Slot B
-          </template>
-        </split-view>
-      </split-view>
-    </div>
+		<div :class="$s.Container">
+			<split-view direction="horizontal">
+				<template slot="A">
+					Slot A
+				</template>
+				<split-view
+					slot="B"
+					direction="vertical"
+				>
+					<template slot="A">
+						Slot A
+					</template>
+					<template slot="B">
+						Slot B
+					</template>
+				</split-view>
+			</split-view>
+		</div>
 
-    <div :class="$s.Container">
-      <split-view
-        direction="horizontal"
-        a-init="100px"
-        a-min="50px"
-        a-max="200px"
-      >
-        <template slot="A">
-          Slot A
-        </template>
-        <split-view
-          slot="B"
-          direction="vertical"
-        >
-          <template slot="A">
-            Slot A
-          </template>
-          <template slot="B">
-            Slot B
-          </template>
-        </split-view>
-      </split-view>
-    </div>
-  </div>
+		<div :class="$s.Container">
+			<split-view
+				direction="horizontal"
+				a-init="100px"
+				a-min="50px"
+				a-max="200px"
+			>
+				<template slot="A">
+					Slot A
+				</template>
+				<split-view
+					slot="B"
+					direction="vertical"
+				>
+					<template slot="A">
+						Slot A
+					</template>
+					<template slot="B">
+						Slot B
+					</template>
+				</split-view>
+			</split-view>
+		</div>
+	</div>
 </template>
 
 <script>
 import SplitView from "./SplitView.vue";
 
 export default {
-  name: "app",
-  components: {
-    SplitView
-  }
+	name: "app",
+	components: {
+		SplitView
+	}
 };
 </script>
 
 <style>
 html,
 body {
-  margin: 0;
+	margin: 0;
 }
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-family: "Avenir", Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
 }
 </style>
 
 
 <style module="$s">
 .Container {
-  border: 1px solid #ccc;
-  width: 501px;
-  height: 500px;
+	border: 1px solid #ccc;
+	width: 501px;
+	height: 500px;
 }
 </style>
--- src/SplitView.vue
+++ src/SplitView.vue
@@ -1,190 +1,190 @@
 <template>
-  <div
-    ref="container"
-    :class="[$s.SplitView, {
-      [$s.isVertical]: isVertical
-    }]"
-  >
-    <div
-      :class="[$s.SideA, {
-        [$s.isLocked]: isDragging
-      }]"
-      :style="styleA"
-    >
-      <slot name="A" />
-    </div>
-    <span
-      :class="[$s.Handle, {
-        [$s.isVertical]: isVertical,
-        [$s.isHorizontal]: !isVertical
-      }]"
-      @mousedown.prevent="dragStart"
-    />
-    <div :class="[$s.SideB, {
-        [$s.isLocked]: isDragging
-      }]">
-      <slot name="B" />
-    </div>
-  </div>
+	<div
+		ref="container"
+		:class="[$s.SplitView, {
+			[$s.isVertical]: isVertical
+		}]"
+	>
+		<div
+			:class="[$s.SideA, {
+				[$s.isLocked]: isDragging
+			}]"
+			:style="styleA"
+		>
+			<slot name="A" />
+		</div>
+		<span
+			:class="[$s.Handle, {
+				[$s.isVertical]: isVertical,
+				[$s.isHorizontal]: !isVertical
+			}]"
+			@mousedown.prevent="dragStart"
+		/>
+		<div :class="[$s.SideB, {
+				[$s.isLocked]: isDragging
+			}]">
+			<slot name="B" />
+		</div>
+	</div>
 </template>
 
 <script>
 function getPosition(element) {
-  let xPosition = 0;
-  let yPosition = 0;
+	let xPosition = 0;
+	let yPosition = 0;
 
-  while (element) {
-    xPosition += element.offsetLeft - element.scrollLeft + element.clientLeft;
-    yPosition += element.offsetTop - element.scrollTop + element.clientTop;
-    element = element.offsetParent;
-  }
+	while (element) {
+		xPosition += element.offsetLeft - element.scrollLeft + element.clientLeft;
+		yPosition += element.offsetTop - element.scrollTop + element.clientTop;
+		element = element.offsetParent;
+	}
 
-  return { x: xPosition, y: yPosition };
+	return { x: xPosition, y: yPosition };
 }
 
 export default {
-  name: "SplitView",
+	name: "SplitView",
 
-  props: {
-    direction: {
-      type: String,
-      default: "horizontal",
-      validator: direction => ["vertical", "horizontal"].includes(direction)
-    },
-    AInit: {
-      type: String,
-      default: "50%"
-    },
-    AMin: {
-      type: String,
-      default: "none"
-    },
-    AMax: {
-      type: String,
-      default: "none"
-    }
-  },
+	props: {
+		direction: {
+			type: String,
+			default: "horizontal",
+			validator: direction => ["vertical", "horizontal"].includes(direction)
+		},
+		AInit: {
+			type: String,
+			default: "50%"
+		},
+		AMin: {
+			type: String,
+			default: "none"
+		},
+		AMax: {
+			type: String,
+			default: "none"
+		}
+	},
 
-  data() {
-    return {
-      isDragging: false,
-      offset: this.AInit
-    };
-  },
+	data() {
+		return {
+			isDragging: false,
+			offset: this.AInit
+		};
+	},
 
-  computed: {
-    isVertical() {
-      return this.direction === "vertical";
-    },
-    offsetA() {
-      if (typeof this.offset === "string") {
-        return this.offset;
-      }
-      return `${this.offset}px`;
-    },
-    styleA() {
-      const property = this.isVertical ? "Height" : "Width";
-      return {
-        [property.toLowerCase()]: this.offsetA,
-        [`min${property}`]: this.AMin,
-        [`max${property}`]: this.AMax
-      };
-    }
-  },
+	computed: {
+		isVertical() {
+			return this.direction === "vertical";
+		},
+		offsetA() {
+			if (typeof this.offset === "string") {
+				return this.offset;
+			}
+			return `${this.offset}px`;
+		},
+		styleA() {
+			const property = this.isVertical ? "Height" : "Width";
+			return {
+				[property.toLowerCase()]: this.offsetA,
+				[`min${property}`]: this.AMin,
+				[`max${property}`]: this.AMax
+			};
+		}
+	},
 
-  methods: {
-    dragStart() {
-      this.isDragging = true;
-      window.addEventListener("mousemove", this.dragging, { passive: true });
-      window.addEventListener("mouseup", this.dragStop, {
-        passive: true,
-        once: true
-      });
-    },
+	methods: {
+		dragStart() {
+			this.isDragging = true;
+			window.addEventListener("mousemove", this.dragging, { passive: true });
+			window.addEventListener("mouseup", this.dragStop, {
+				passive: true,
+				once: true
+			});
+		},
 
-    dragStop() {
-      window.removeEventListener("mousemove", this.dragging);
-      this.isDragging = false;
-    },
+		dragStop() {
+			window.removeEventListener("mousemove", this.dragging);
+			this.isDragging = false;
+		},
 
-    mouseOffset(e) {
-      const { container } = this.$refs;
-      const containerOffset = getPosition(container);
-      let offset;
+		mouseOffset(e) {
+			const { container } = this.$refs;
+			const containerOffset = getPosition(container);
+			let offset;
 
-      if (this.isVertical) {
-        offset = e.pageY - containerOffset.y;
-        offset = Math.min(offset, container.offsetHeight);
-      } else {
-        offset = e.pageX - containerOffset.x;
-        offset = Math.min(offset, container.offsetWidth);
-      }
+			if (this.isVertical) {
+				offset = e.pageY - containerOffset.y;
+				offset = Math.min(offset, container.offsetHeight);
+			} else {
+				offset = e.pageX - containerOffset.x;
+				offset = Math.min(offset, container.offsetWidth);
+			}
 
-      return Math.max(offset, 0);
-    },
+			return Math.max(offset, 0);
+		},
 
-    dragging(e) {
-      this.offset = this.mouseOffset(e);
-    }
-  }
+		dragging(e) {
+			this.offset = this.mouseOffset(e);
+		}
+	}
 };
 </script>
 
 <style module="$s">
 .SplitView {
-  position: relative;
-  display: flex;
-  width: 100%;
-  height: 100%;
+	position: relative;
+	display: flex;
+	width: 100%;
+	height: 100%;
 
-  &.isVertical {
-    flex-direction: column;
-  }
+	&.isVertical {
+		flex-direction: column;
+	}
 }
 
 .Handle {
-  user-select: none;
-  box-sizing: border-box;
-  transition: all 0.3s ease;
-  z-index: 1;
-  background: padding-box #000a;
+	user-select: none;
+	box-sizing: border-box;
+	transition: all 0.3s ease;
+	z-index: 1;
+	background: padding-box #000a;
 
-  &.isHorizontal {
-    width: 11px;
-    border-left: 5px solid transparent;
-    border-right: 5px solid transparent;
-    margin: 0 -5px;
-    cursor: col-resize;
-  }
+	&.isHorizontal {
+		width: 11px;
+		border-left: 5px solid transparent;
+		border-right: 5px solid transparent;
+		margin: 0 -5px;
+		cursor: col-resize;
+	}
 
-  &.isVertical {
-    height: 11px;
-    border-top: 5px solid transparent;
-    border-bottom: 5px solid transparent;
-    margin: -5px 0;
-    cursor: row-resize;
-  }
+	&.isVertical {
+		height: 11px;
+		border-top: 5px solid transparent;
+		border-bottom: 5px solid transparent;
+		margin: -5px 0;
+		cursor: row-resize;
+	}
 
-  &:hover,
-  &:active {
-    border-color: #0004;
-  }
+	&:hover,
+	&:active {
+		border-color: #0004;
+	}
 
-  &:active {
-    border-width: 4px;
-  }
+	&:active {
+		border-width: 4px;
+	}
 }
 
 .SideA {
-  overflow: auto;
+	overflow: auto;
 
-  &.isLocked {
-    pointer-events: none;
-  }
+	&.isLocked {
+		pointer-events: none;
+	}
 }
 
 .SideB {
-  composes: SideA;
-  flex: 1;
+	composes: SideA;
+	flex: 1;
 }
 </style>
