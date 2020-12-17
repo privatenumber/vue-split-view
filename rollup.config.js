634ea1c897befe3f8cf92d0aa664437324a0ef47
Hiroki Osame <hiroki.osame@gmail.com>
2020-12-17 16:19:09 -0500

    feat: support vue 2 and 3

--- rollup.config.js
+++ rollup.config.js
@@ -1,39 +1,70 @@
 import babel from 'rollup-plugin-babel';
 import {terser} from 'rollup-plugin-terser';
 import filesize from 'rollup-plugin-filesize';
-import vue from 'rollup-plugin-vue';
 import postcss from 'rollup-plugin-postcss'
 import presetEnv from 'postcss-preset-env';
+import vue2 from 'rollup-plugin-vue2';
+import vue3 from 'rollup-plugin-vue3';
 
 const isProd = process.env.NODE_ENV === 'production';
 
-const rollupConfig = {
-	input: 'src/SplitView.vue',
-	plugins: [
-		vue({
-			css: false,
-		}),
-		postcss({
-			extract: 'split-view.css',
-			plugins: [
-				presetEnv({ stage: 0, }),
-			],
-		}),
-		babel(),
-		isProd && terser(),
-		isProd && filesize(),
-	],
-	output: [
-		{
-			format: 'es',
-			file: 'dist/split-view.esm.js',
-		},
-		{
-			format: 'umd',
-			file: 'dist/split-view.umd.js',
-			name: 'SplitView',
-		},
-	],
-};
+const rollupConfig = [
+	{
+		input: 'src/SplitView.vue',
+		plugins: [
+			vue2({
+				css: false,
+			}),
+			postcss({
+				extract: 'split-view.css',
+				plugins: [
+					presetEnv({ stage: 0, }),
+				],
+			}),
+			babel(),
+			isProd && terser(),
+			isProd && filesize(),
+		],
+		output: [
+			{
+				format: 'es',
+				file: 'dist/split-view.vue2.esm.js',
+			},
+			{
+				format: 'umd',
+				file: 'dist/split-view.vue2.umd.js',
+				name: 'SplitView',
+			},
+		],
+	},
+	{
+		input: 'src/SplitView.vue',
+		plugins: [
+			vue3({
+				css: false,
+			}),
+			postcss({
+				extract: 'split-view.css',
+				plugins: [
+					presetEnv({ stage: 0, }),
+				],
+			}),
+			babel(),
+			isProd && terser(),
+			isProd && filesize(),
+		],
+		output: [
+			{
+				format: 'es',
+				file: 'dist/split-view.vue3.esm.js',
+			},
+			{
+				format: 'umd',
+				file: 'dist/split-view.vue3.umd.js',
+				name: 'SplitView',
+			},
+		],
+	}
+];
 
 export default rollupConfig;
