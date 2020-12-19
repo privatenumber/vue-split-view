e92189397accb483095fac0f68a95aa1c7d8fde9
hiroki <hiroki.osame@gmail.com>
2020-12-19 05:50:52 -0500

    feat: support vue 2 and 3 distribution (#9)

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,26 @@
 import babel from 'rollup-plugin-babel';
 import {terser} from 'rollup-plugin-terser';
 import filesize from 'rollup-plugin-filesize';
-import vue from 'rollup-plugin-vue';
 import postcss from 'rollup-plugin-postcss'
 import presetEnv from 'postcss-preset-env';
+import vue2 from 'rollup-plugin-vue2';
+import vue3 from 'rollup-plugin-vue3';
 
 const isProd = process.env.NODE_ENV === 'production';
 
-const rollupConfig = {
+const rollupConfig = [
+	{
+		label: 'vue2',
+		plugin: vue2,
+	},
+	{
+		label: 'vue3',
+		plugin: vue3,
+	},
+].map(({ label, plugin }) => ({
 	input: 'src/SplitView.vue',
 	plugins: [
-		vue({
+		plugin({
 			css: false,
 		}),
 		postcss({
@@ -23,17 +33,21 @@ const rollupConfig = {
 		isProd && terser(),
 		isProd && filesize(),
 	],
+	external: 'vue',
 	output: [
 		{
 			format: 'es',
-			file: 'dist/split-view.esm.js',
+			file: `dist/split-view.${label}.esm.js`,
 		},
 		{
 			format: 'umd',
-			file: 'dist/split-view.umd.js',
+			file: `dist/split-view.${label}.umd.js`,
 			name: 'SplitView',
+			globals: {
+				vue: 'vue',
+			},
 		},
 	],
-};
+}));
 
 export default rollupConfig;
