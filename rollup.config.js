58b0fb368056ae0ea492ad2b5f7643f896df16f6
Hiroki Osame <hiroki.osame@gmail.com>
2020-12-27 15:56:01 -0500

    chore: move aggreate-exports to module

--- rollup.config.js
+++ rollup.config.js
@@ -3,30 +3,11 @@ import { terser } from 'rollup-plugin-terser';
 import filesize from 'rollup-plugin-filesize';
 import postcss from 'rollup-plugin-postcss';
 import presetEnv from 'postcss-preset-env';
+import aggregateExports from 'rollup-plugin-aggregate-exports';
 import vue2 from 'rollup-plugin-vue2';
 import vue3 from 'rollup-plugin-vue3';
 
 const isProduction = process.env.NODE_ENV === 'production';
-
-// TODO extract out to plugin
-const aggregateExports = options => ({
-	name: 'aggregated-exports',
-	generateBundle() {
-		this.emitFile({
-			fileName: options.name,
-			type: 'asset',
-			source: options.exports
-				.map((expFrom) => {
-					if (typeof expFrom === 'string') {
-						return `export * from '${expFrom}'`;
-					}
-					const identifiers = (expFrom.identifiers || [expFrom.identifier]).join(',');
-					return `export {${identifiers}} from '${expFrom.from}'`;
-				})
-				.join(';'),
-		});
-	},
-});
 
 const rollupConfig = [
 	{
@@ -56,7 +37,7 @@ const rollupConfig = [
 		isProduction && terser(),
 		isProduction && filesize(),
 		aggregateExports({
-			name: `${label}.js`,
+			fileName: `${label}.js`,
 			exports: [
 				{
 					identifier: 'default',
