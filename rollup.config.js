5d4f1941d38e0bcbbc5f2be4eb44f3799435d028
hiroki <hiroki.osame@gmail.com>
2020-12-29 15:39:14 -0500

    chore: dev env (#12)

--- rollup.config.js
+++ rollup.config.js
@@ -2,16 +2,13 @@ import babel from '@rollup/plugin-babel';
 import { terser } from 'rollup-plugin-terser';
 import filesize from 'rollup-plugin-filesize';
 import postcss from 'rollup-plugin-postcss';
-import presetEnv from 'postcss-preset-env';
 import aggregateExports from 'rollup-plugin-aggregate-exports';
 import vue2 from 'rollup-plugin-vue2';
 import vue3 from 'rollup-plugin-vue3';
 
-const isProduction = process.env.NODE_ENV === 'production';
+const { plugins: postcssPlugins } = require('./postcss.config.js');
 
-const postcssPlugins = [
-	presetEnv({ stage: 0 }),
-];
+const isProduction = process.env.NODE_ENV === 'production';
 
 const rollupConfig = [
 	{
